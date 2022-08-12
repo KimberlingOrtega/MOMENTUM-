9d6e6a0042f77a84a42ab34a4d89ee2ded8870f8
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-08-12 23:57:11 +0000

    todos los archivos integrados

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// export const Navbar = () => {
+// 	return (
+// 		<nav className="navbar navbar-light bg-light">
+// 			<div className="container">
+// 				<Link to="/">
+// 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+// 				</Link>
+// 				<div className="ml-auto">
+// 					<Link to="/demo">
+// 						<button className="btn btn-primary">Check the Context in action</button>
+// 					</Link>
+// 				</div>
+// 			</div>
+// 		</nav>
+
+// 	);
+
+// };
+
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="fixed top-0 left-0 rigth-0 text-3xl bg-blue-500 w-100 text-center">
+      Nombre del fotografo
+    </div>
+  );
 };
