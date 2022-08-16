4e000616ee3f6ba20fddae4f1159c07cab6f4983
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-08-16 00:19:02 +0000

    algo borrado

--- src/front/js/component/Modal.js
+++ src/front/js/component/Modal.js
@@ -127,28 +127,6 @@ export const Modal = () => {
               </form>
             </div>
             <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-              {/* <button
-                type="button"
-                className="px-6
-          py-2.5
-          bg-purple-600
-          text-white
-          font-medium
-          text-xs
-          leading-tight
-          uppercase
-          rounded
-          shadow-md
-          hover:bg-purple-700 hover:shadow-lg
-          focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-purple-800 active:shadow-lg
-          transition
-          duration-150
-          ease-in-out"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button> */}
               <button
                 type="button"
                 className="px-6
