88ad49d48c4a5ad1aee0e995956f95e51613bbb3
robertodefm <robertodefm@gmail.com>
2022-08-18 22:24:57 +0000

    de nuevo a tailwind

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 import Logo from "../../img/logo-fotografo.png";
 import { Login } from "./Login";
 
-// ¡¡TAILWIND!!
-// export const Navbar = () => {
-//   return (
-//     <div className="relative">
-//       <div className="shadow-md w-full fixed top-0 flex justify-center bg-white opacity-80">
-//         <div className="">
-//           <img className="object-cover w-40 h-20" src={Logo} />
-//         </div>
-//       </div>
-//       <div className="absolute right-0 flex items-center h-20 mx-4">
-//         <Login />
-//       </div>
-//     </div>
-//   );
-// };
+¡¡TAILWIND!!
+export const Navbar = () => {
+  return (
+    <div className="relative">
+      <div className="shadow-md w-full fixed top-0 flex justify-center bg-white opacity-80">
+        <div className="">
+          <img className="object-cover w-40 h-20" src={Logo} />
+        </div>
+      </div>
+      <div className="absolute right-0 flex items-center h-20 mx-4">
+        <Login />
+      </div>
+    </div>
+  );
+};
 
 // export const Navbar = () => {
 // 	return (
@@ -38,15 +38,16 @@ import { Login } from "./Login";
 
 // };
 
-export const Navbar = () => {
-  return (
-    <nav class="navbar bg-opacity-75 py-0 fixed-top bg-white d-flex mx-auto container-fluid">
-      <div class="container w-40 h-20 px-0">
-        <img className=" img-fluid " src={Logo} />
-      </div>
-      <div className="position-absolute end-0 me-2">
-        <Login />
-      </div>
-    </nav>
-  );
-};
+// BOOTSTRAP
+// export const Navbar = () => { 
+//   return (
+//     <nav class="navbar bg-opacity-75 py-0 fixed-top bg-white d-flex mx-auto container-fluid">
+//       <div class="container w-40 h-20 px-0">
+//         <img className=" img-fluid " src={Logo} />
+//       </div>
+//       <div className="position-absolute end-0 me-2">
+//         <Login />
+//       </div>
+//     </nav>
+//   );
+// };
