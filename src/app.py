200f75a384259bc773572f2de87d301bb9ae47e2
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-08-22 02:00:22 +0000

    añadido jwt manager

--- src/app.py
+++ src/app.py
@@ -9,15 +9,17 @@ from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
-from api.admin import setup_admin
 from api.commands import setup_commands
-
+from api.admin import setup_admin
+from flask_jwt_extended import JWTManager
 #from models import Person
 
-ENV = os.getenv("FLASK_ENV")
+ENV = os.getenv("FLASK_DEBUG")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -27,6 +29,7 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
 
