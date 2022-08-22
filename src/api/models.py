960b62aad41972ec0b0fc46833b395281663d90c
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-08-22 23:40:35 +0000

    tablas

--- src/api/models.py
+++ src/api/models.py
@@ -4,14 +4,14 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(120), unique=False, nullable=False)
-    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    full_name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(250), unique=False, nullable=False)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
     salt = db.Column(db.String(80), nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean, unique=False, nullable=False)
     orders = db.relationship('UserOrder', backref='user', uselist=True)   #RELACION,TABLA USER-ORDER:
-   
+    admin = db.Column(db.Boolean, nullable=True)
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -19,8 +19,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
+            "full_name": self.full_name,
+            "phone_number": self.phone_number,
             "salt": self.salt
             # do not serialize the password, its a security breach
         }
@@ -48,4 +48,15 @@ class UserOrder(db.Model): #TABLA DE ORDEN
     def serialize(self):
         return{
             "id": self.id
-        }      
+        } 
+           
+class Cotizacion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    service = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(400), nullable=False)
+              
+        
+     
