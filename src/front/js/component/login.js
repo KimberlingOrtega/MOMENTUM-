a95c95d1b1a587a2600d06c282f91fc78bcdd5c5
robertodefm <robertodefm@gmail.com>
2022-08-24 20:08:14 +0000

    centrado modal login y ajustes boton

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -6,7 +6,7 @@ export const Login = () => {
   return (
     <div>
       <button
-        className=" block text-white bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className=" block text-white titlecomponent  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
         data-modal-toggle="authentication-modal"
         onClick={() => {
@@ -15,7 +15,7 @@ export const Login = () => {
       >
         Login
       </button>
-
+      
       <div
         id="authentication-modal"
         tabIndex="-1"
@@ -24,8 +24,8 @@ export const Login = () => {
           isShow ? "" : "hidden"
         } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full`}
       >
-        <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div className="relative p-4 mx-auto mt-32 w-full max-w-md h-full md:h-auto">
+          <div className="relative bg-gray-100 rounded-lg shadow dark:bg-gray-700">
             <button
               type="button"
               className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
@@ -103,28 +103,14 @@ export const Login = () => {
                       Remember me
                     </label>
                   </div>
-                  <a
-                    href="#"
-                    className="text-sm text-blue-700 hover:underline dark:text-blue-500"
-                  >
-                    Lost Password?
-                  </a>
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="titlecomponent w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Login to your account
                 </button>
-                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Not registered?{" "}
-                  <a
-                    href="#"
-                    className="text-blue-700 hover:underline dark:text-blue-500"
-                  >
-                    Create account
-                  </a>
-                </div>
+
               </form>
             </div>
           </div>
