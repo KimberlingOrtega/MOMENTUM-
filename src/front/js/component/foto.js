57a3768c816347a6b7c0907564fdae113704f7fc
XavierSilvaB <xavieraugustosilvabaute@gmail.com>
2022-08-25 02:17:49 +0000

    Actualizacion de Galerias

--- src/front/js/component/foto.js
+++ src/front/js/component/foto.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Foto = (props) => {
   return (
-    <div className="p-0 m-0 rounded-lg shadow-lg w-30  ">
-      <img className="w-full" src={props.url} />
+    <div className="p-0 m-0 rounded-lg shadow-lg  ">
+      <img className="icon" src={props.url} />
     </div>
   );
 };
---
+++ src/front/js/component/fotoGallery.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const FotoGallery = (props) => {
+  return (
+    <div className="row ">
+      <div className="column ">
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/Bresaola-MOTTOLINI-2013-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-TIPICO-BRANZI-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZA-LEGGERA-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Calusco-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/VANIGLIA-QUINOA-768x512.jpg.webp" />
+      </div>
+      <div className="column ">
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-BRANZI-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/OSTERIA-DEL-BORGO-Bergamo-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA_Bergamo-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Calusco-Adda-768x512.jpg.webp" />
+      </div>
+      <div className="column ">
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-BRANZI-OROBIE-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/OSTERIA-DEL-BORGO-Oneta-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA_Calusco-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-menu-768x512.jpg.webp" />
+      </div>
+      <div className="column ">
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/Formaggio-delle-Orobie-BRANZI-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Bergamo-768x512.jpg.webp" />
+        <img src="https://www.matteozanga.it/wp-content/uploads/2020/05/VANIGLIA-e-QUINOA-768x512.jpg.webp" />
+      </div>
+    </div>
+  );
+};
