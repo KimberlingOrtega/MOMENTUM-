d9ebd521776f76c21df7d298c8588f6d02729281
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-08-25 21:56:14 +0000

    componente admin

--- src/front/js/component/adminmenu.js
+++ src/front/js/component/adminmenu.js
@@ -1,22 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Adminmenu = ({admin}) => {
+export const Adminmenu = ({ admin }) => {
   return (
     <div>
       <div className="dropdown">
-  <button className="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    {admin}
-  </button>
-  <ul className="dropdown-menu">
-    <li><Link className="dropdown-item" to="/datos">Datos</Link></li>
-    <li><Link className="dropdown-item" to="/clientes">Clientes</Link></li>
-    <li><Link className="dropdown-item" to="/admin">Trabajos realizados</Link></li>
-    <li><hr class="dropdown-divider"/></li>
-    <li><a className="dropdown-item" href="#">Salir</a></li>
-
-  </ul>
-</div>
+        <button
+          className="btn  dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {admin}
+        </button>
+        <ul className="dropdown-menu">
+          <li>
+            <Link className="dropdown-item" to="/datos">
+              Datos
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="/clientes">
+              Clientes
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="/admin">
+              Trabajos realizados
+            </Link>
+          </li>
+          <li>
+            <hr className="dropdown-divider" />
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Salir
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
