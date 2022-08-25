0e4470f96a655bebe18db25f78e900afaf1ed8f0
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-08-25 21:57:38 +0000

    componente user menu modificado

--- src/front/js/component/usermenu.js
+++ src/front/js/component/usermenu.js
@@ -1,24 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-export const Usermenu = ({cliente}) => {
+export const Usermenu = ({ cliente }) => {
   return (
     <div>
       <div className="dropdown">
-  <button className="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    {cliente}
-  </button>
-  <ul className="dropdown-menu">
-    <li><Link className="dropdown-item" to="/datos">Datos</Link></li>
-    <li><Link className="dropdown-item" to="/trabajos">Trabajos realizados</Link></li>
-    <li><Link className="dropdown-item" to="/cotizacion">Nueva cotización</Link></li>
-    <li><hr class="dropdown-divider"/></li>
-    <li><a className="dropdown-item" href="#">Salir</a></li>
-
-  </ul>
-</div>
+        <button
+          className="btn  dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {cliente}
+        </button>
+        <ul className="dropdown-menu">
+          <li>
+            <Link className="dropdown-item" to="/datos">
+              Datos
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="/trabajos">
+              Trabajos realizados
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="/cotizacion">
+              Nueva cotización
+            </Link>
+          </li>
+          <li>
+            <hr className="dropdown-divider" />
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Salir
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
-
