e056c7cac7d04ba32eb41da23c394aa1bf71439c
robertodefm <robertodefm@gmail.com>
2022-08-25 16:55:36 +0000

    eventos listo y description con props

--- src/front/js/component/description.js
+++ src/front/js/component/description.js
@@ -1,23 +1,17 @@
 import React from "react";
 
-export const Description = () => {
+export const Description = (props) => {
   return (
     <>
       <div className="  h-80  pt-6">
-        <p className="text-title text-3xl flex justify-center items-center h-1/4 w-6/12 mx-auto bg-white mb-2 text-slate-500 font-bold">
-          Food, Beverage, Still Life ... the good to photograph
+        <p className="text-title text-6xl flex justify-center items-center h-1/4 w-6/12 mx-auto bg-white mb-2 text-slate-500 font-bold">
+          {props.title}
         </p>
-        <hr className="hr-line" />
-        <p className="text-title text-xl flex justify-center items-center w-7/12 mx-auto text-teal-600 font-semibold h-1/4 px-16 mt-3 mb-3">
-          Commercial images for restaurants, menus, catalogs, cookbooks, food
-          companies, producers, farms, blogs and food sector publishing.
+        <hr className="hr-line mb-12" />
+        <p className="text-title text-4xl flex justify-center items-center w-7/12 mx-auto text-gray-400 font-semibold h-1/4 px-16 mt-3 mb-3">
+        {props.text}
         </p>
-        <p className="text-title text-slate-500 font-semibold text-xl flex justify-center items-center w-8/12 h-1/4 mx-auto  mt-2">
-          Food is one of the great joys of life, and I love helping
-          restaurateurs and producers to display and enhance their works of art.
-          If you want to "eat with your eyes" these delicious dishes, then I
-          will have succeeded in my intent!
-        </p>
+
       </div>
     </>
   );
--- src/front/js/pages/evento.js
+++ src/front/js/pages/evento.js
@@ -6,22 +6,95 @@ import { Modal } from "../component/modal";
 export const Evento = () => {
   return (
     <div className="px-0 pt-32 mx-auto ">
-      <Title titulo="Evento" />
-      <Description />
+      <Title titulo="Eventos" />
+      <Description title="Fotografía de eventos" text="Siempre habrá eventos especiales que deben ser capturados por un fotógrafo profesional. Y eso va desde fiestas de cumpleaños infantiles hasta conciertos o eventos nocturnos."/>
 
-      <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-3 gap-2 font-mono text-white text-sm text-center font-bold leading-6 bg-stripes-fuchsia rounded-lg p-0 mx-6">
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/AMAZING.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/AMAZING-MODA.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/MORGANA_GIOIELLI.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
+      <div className="row ">
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/amigos-sonrientes-tiro-medio-celebrando-cumpleanos_52683-88945.jpg?w=1480&t=st=1661445494~exp=1661446094~hmac=5a1fb4732f75a009d2a21cc31aa4e5554fe6e5d4bee97b38476b3c3c1b9a174d"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/amigos-tintinean-vasos-bebida-bar-moderno_1150-18971.jpg?w=1480&t=st=1661445528~exp=1661446128~hmac=aaee2fc50fd605c49be2cfdbacb4504530dae3a215f3c7db37289a3e895aba84"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/gente-vida-nocturna-divirtiendose-bares-discotecas_23-2149088568.jpg?w=1480&t=st=1661445556~exp=1661446156~hmac=a9233453afedd7c020c15c402f8b0905523ef5f66e3d475b04ca342022acfe5c"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/coctel-azul-frio-bola-discoteca_144627-24450.jpg?w=740&t=st=1661445576~exp=1661446176~hmac=5a4d9d88e8e0fcf68b0a098a077f6c6bfa97a7dd017f5a8b4f8f2b7bdc842c76"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/fotos-premium/multitud-personas-pista-baile-luces-rojas-azules_106386-249.jpg?w=1480"
+          />
+        </div>
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/vista-frontal-mujer-celebrando-ano-nuevo_23-2148722190.jpg?w=740&t=st=1661445668~exp=1661446268~hmac=33ee703c4c1259554c02e09c98966181ab69d9ed5d9aeeeef5c0a75fef06c8fd"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/encantadora-mujer-bronceada-levantando-copa-vino_197531-13034.jpg?w=1480&t=st=1661445707~exp=1661446307~hmac=3734bd8593a638d3b7273d60f9693a29e75b51851b1e10648b48b306c978298a"
+          />
+            <Foto
+              className="icon"
+              url="https://img.freepik.com/foto-gratis/tres-parejas-celebrando-fiesta_329181-5719.jpg?w=1480&t=st=1661445740~exp=1661446340~hmac=16302ec47b6e89ac47386c8c2e777ce1b02ffd9b1e534ef6c4b45782700dc19e"
+            />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/vista-frontal-sonriente-mujer-sosteniendo-bebida_23-2149631342.jpg?w=740&t=st=1661445790~exp=1661446390~hmac=d6c245ce6a4fe370dc0f340ad11922d19dc097aed50b1d84b566ed818475c9c6"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/vista-posterior-multitud-fans-viendo-actuaciones-directo-concierto-musica-noche-copiar-espacio_637285-544.jpg?w=2000&t=st=1661445874~exp=1661446474~hmac=2fa083d6ffe60e3b943a62e7cd6820ace93aa028cdf89fa431bce5449dc90202"
+          />
+        </div>
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/fiesta-dj_74658-146.jpg?w=1480&t=st=1661445917~exp=1661446517~hmac=d9faac075c952f1f213020ac37b0fcb70442758d0e7be9739f9c158ea1963969"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/mujer-dj-alto-angulo-panel-control_23-2148325488.jpg?w=740&t=st=1661445969~exp=1661446569~hmac=f14b3d34cc321af18c28e9c5ee63d4750cace59100a1d168fb18143b5682bd90"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/dj-tocando-musica-mezclador-closeup_93675-132649.jpg?w=740&t=st=1661446022~exp=1661446622~hmac=9f7c9b1ba64b81ff2992b7a8f540587f2965e6bd3576182eeb8f5183476fce51"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/espectaculo-laser-lineas-brillantes-luces-neon-fondo-negro_181624-10088.jpg?w=2000&t=st=1661446060~exp=1661446660~hmac=34914ba8f5e74772cf6740a5c2ce1f8d2a4931b72179170cd61d21cd7a8b7668"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/audiencia-emocionada-viendo-fuegos-artificiales-confeti-divirtiendose-festival-musica-noche-copiar-espacio_637285-559.jpg?w=1480&t=st=1661446121~exp=1661446721~hmac=3046f040c41d122f1d9497ae66269f71de37e14ec88e57aa8637006372803eaa"
+          />
+        </div>
+
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/retrato-cuerpo-entero-pareja-abrazada-globo-fiesta_329181-5352.jpg?w=740&t=st=1661446206~exp=1661446806~hmac=f204f2732a4bc03fef3795a21125243b303281d60e5d7655495bf4e0eafe46e0"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/cuatro-mujeres-jovenes-bengalas-encendidas-puesta-sol_329181-18008.jpg?w=1480&t=st=1661446169~exp=1661446769~hmac=56d687d78134554fd6d2ab1840a408907922929133f19137e1412cfc71f7b660"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/familia-feliz-tiro-completo-decorando-arbol_23-2149183259.jpg?w=740&t=st=1661446258~exp=1661446858~hmac=86eb45c1f0d7e688e0d094721255246e28b7bc8dfe8a3ac802253da91fee6e59"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/lindo-perro-pastor-gorro-papa-noel-casa_181624-52967.jpg?w=740&t=st=1661446284~exp=1661446884~hmac=936682680fd57ff0c0cd973c99a894ae110247cfab1f09f1448be55d4ef93d14"
+          />
+        </div>
       </div>
       <Modal />
     </div>
