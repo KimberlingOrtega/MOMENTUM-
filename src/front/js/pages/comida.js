430b6b4d985c302b202e59ed163c5dd8131b1a11
robertodefm <robertodefm@gmail.com>
2022-08-30 22:48:16 +0000

    correcion de vistas (imagenes y texto)

--- src/front/js/pages/comida.js
+++ src/front/js/pages/comida.js
@@ -8,87 +8,97 @@ export const Comida = () => {
   return (
     <div className="px-0 pt-32 mx-auto ">
       <Title titulo="Comida" />
-      <Description
-        title="Alimentos, Bebidas, Bodegones... lo bueno para fotografiar."
-        text="imágenes comerciales para restaurantes, menús, catálogos, recetarios, empresas de alimentación, productores, granjas, blogs y publicaciones del sector alimento."
-      />
+      <Description title="Fotografía de comida" text="La comida es una de las grandes alegrías de la vida, y me encanta ayudar a los restauradores y productores a mostrar y realzar sus obras de arte.
+Si quieres comer con los ojos estos deliciosos platos, ¡entonces habré tenido éxito en mi intento!"/>
 
       <div className="row ">
         <div className="column ">
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Bresaola-MOTTOLINI-2013-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Bresaola-MOTTOLINI-2013.jpg"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-TIPICO-BRANZI-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Formaggio-delle-Orobie-BRANZI.jpg"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZA-LEGGERA-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/OSTERIA-DEL-BORGO-Oneta.jpg"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Calusco-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA_Bergamo.jpg"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/VANIGLIA-QUINOA-768x512.jpg.webp"
-          />
-        </div>
-        <div className="column ">
-          <Foto
-            className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-BRANZI-768x512.jpg.webp"
-          />
-          <Foto
-            className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/OSTERIA-DEL-BORGO-Bergamo-768x512.jpg.webp"
-          />
-          <Foto
-            className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA_Bergamo-768x512.jpg.webp"
-          />
-          <Foto
-            className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Calusco-Adda-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Calusco.jpg"
           />
         </div>
         <div className="column ">
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-BRANZI-OROBIE-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-BRANZI.jpg"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/OSTERIA-DEL-BORGO-Oneta-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-TIPICO-BRANZI.jpg"
+          />
+            <Foto
+              className="icon"
+              url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA.jpg"
+            />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/FORMAGGIO-BRANZI-OROBIE.jpg"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA_Calusco-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Calusco-Adda.jpg"
+          />
+        </div>
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/VANIGLIA-e-QUINOA.jpg"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-menu-768x512.jpg.webp"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-menu.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/VANIGLIA-QUINOA.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Bergamo.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/OSTERIA-DEL-BORGO-Bergamo.jpg"
           />
         </div>
 
         <div className="column ">
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Formaggio-delle-Orobie-BRANZI-768x512.jpg.webp"
+            url="https://img.freepik.com/foto-gratis/closeup-foto-deliciosa-pasta-plato-blanco_181624-42152.jpg?w=1480&t=st=1661896756~exp=1661897356~hmac=f7d94fdec70be36d31198292b6ef77012cda404b35382920dcf3691c43a2bc6e"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-768x512.jpg.webp"
+            url="https://img.freepik.com/foto-gratis/placa-albahaca-cereza-menu-gourmet_1220-1184.jpg?w=1480&t=st=1661896779~exp=1661897379~hmac=e3f402087a4e4f182d13a5c259db020783c8fd2ad3b0ff9d41c2477e735c79c1"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PIZZALEGGERA-Bergamo-768x512.jpg.webp"
+            url="https://img.freepik.com/fotos-premium/pizza-mesa-madera-vista-superior-publicacion-comida-rapida-blog-redes-sociales-espacio-copia-pizza-lista-comer_211786-2680.jpg?w=1480"
           />
           <Foto
             className="icon"
-            url="https://www.matteozanga.it/wp-content/uploads/2020/05/VANIGLIA-e-QUINOA-768x512.jpg.webp"
+            url="https://img.freepik.com/foto-gratis/tacos-mexicanos-carne-res-salsa-tomate-salsa_2829-14218.jpg?w=1480&t=st=1661896834~exp=1661897434~hmac=91f45750b5943a30c6d1434455759d5fdd282a101d6874d9c40c711a2c1d9a21"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/paella-espanola-mariscos-mejillones-camarones-etc-paellera-acero-canarias-cocina-pequeno-restaurante-familiar_1217-1763.jpg?w=1480&t=st=1661896907~exp=1661897507~hmac=cf1ef543a5812c9732af349dfaf6cda749001e32f05f287a30241ab5ea08e628"
           />
         </div>
       </div>
--- src/front/js/pages/corporativo.js
+++ src/front/js/pages/corporativo.js
@@ -7,21 +7,101 @@ export const Corporativo = () => {
   return (
     <div className="px-0 pt-32 mx-auto ">
       <Title titulo="Corporativo" />
-      <Description />
+      <Description title="Fotografía de comida" text="Me gusta ayudar a los emprendedores a potenciar sus empresas y sus productos .
+Contar con fotografías de alta calidad significa comunicarse profesionalmente en un entorno cada día más competitivo.
+Trabajadores, maquinaria, etapas de producción, productos terminados:
+¡Muéstrale al mundo de lo que eres capaz!"/>
 
-      <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-3 gap-2 font-mono text-white text-sm text-center font-bold leading-6 bg-stripes-fuchsia rounded-lg p-0 mx-6">
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/AMAZING.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/AMAZING-MODA.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/MORGANA_GIOIELLI.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
-        <Foto url="https://www.matteozanga.it/wp-content/uploads/2020/05/GOLDEN.jpg" />
+      <div className="row ">
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Fotografia-Industriale-Verona.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PHONETICA-MILANO.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/STOCKLI-APPAREL_SLOVAKIA.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/TECNOCOPERTURE.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/DENTAL3-Centro-Odontoiatrico.jpg"
+          />
+        </div>
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Fotografia-Industriale-Bergamo.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Fotografia-Studi-Dentistici.jpg"
+          />
+            <Foto
+              className="icon"
+              url="https://www.matteozanga.it/wp-content/uploads/2020/05/Fotografia-Industriale-Milano.jpg"
+            />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/STOCKLI-APPAREL-SLOVAKIA.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/DENTAL3.jpg"
+          />
+        </div>
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/TECNOCOPERTURE-Bergamo.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/PHONETICA.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/STOCKLI-APPAREL.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/STOCKLI-SLOVAKIA.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Fotografia-Industriale.jpg"
+          />
+        </div>
+
+        <div className="column ">
+          <Foto
+            className="icon"
+            url="https://www.matteozanga.it/wp-content/uploads/2020/05/Fotografia-Industriale-Como.jpg"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/cooperacion-ingenieros-tecnico-masculino-femenino-control-mantenimiento-rele-sistema-brazo-robotico-soldadura-tableta-portatil-controlar-calidad-operacion-proceso-trabajo-industria-pesada-40-fabrica-fabricacion_609648-792.jpg?w=1480&t=st=1661899355~exp=1661899955~hmac=3478bb87998217f1e0348542ea6c770df14f24785a895e9a2824a7a7dcb07fa4"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/hombre-casco-blanco-cerca-panel-solar_1157-29994.jpg?w=740&t=st=1661899424~exp=1661900024~hmac=c17720b955623f3696aa50cb0b753924d19bc8fe61f0c674a60ed41dabb017c7"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/hombre-trabajando-soplete_181624-47275.jpg?w=1480&t=st=1661899476~exp=1661900076~hmac=795d8d3a4ccedc2c0534704fc184d26874f81b494a529a6ed75b3cca27d12a92"
+          />
+          <Foto
+            className="icon"
+            url="https://img.freepik.com/foto-gratis/trabajador-fabrica-casco-uniforme-mostrando-nuevos-productos-metalicos-al-supervisor-gerente_342744-116.jpg?w=1480&t=st=1661899494~exp=1661900094~hmac=3ada49118c268abef4e3f222b922b980e4868f28444ba53674174db5f2b60fdc"
+          />
+        </div>
       </div>
       <Modal />
     </div>
