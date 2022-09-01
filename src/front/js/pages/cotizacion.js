007942cf98fd081087e101c2ac953ea970838f76
XavierSilvaB <xavieraugustosilvabaute@gmail.com>
2022-09-01 01:15:45 +0000

    Actualizacion del componente contacto

--- src/front/js/pages/cotizacion.js
+++ src/front/js/pages/cotizacion.js
@@ -2,22 +2,14 @@ import React from "react";
 import { Form } from "../component/form";
 import { Title } from "../component/title";
 import Places from "./places";
-
+import { Contacto } from "../component/contacto";
 export const Cotizacion = () => {
   return (
     <div className="px-0 pt-32">
       <Title titulo="Cotización" />
       <div className=" flex h-screen ">
         <div className="grow bg-white-400 w-2/4 pt-4 ">
-          <p className="pt-4 text-title text-3xl flex justify-center items-center  w-6/12 mx-auto  mb-2 text-slate-500 font-bold">
-            Sándra Sanchez
-          </p>
-          <p className="text-title text-xl flex justify-center items-center w-10/12 mx-auto text-teal-600 font-semibold  px-16 mt-3 mb-3"></p>
-          <p className="text-title text-xl flex justify-center items-center w-10/12 mx-auto font-semibold  px-16 mt-3 mb-3">
-            <i className="fab fa-linkedin text-5xl"></i>
-            <i className="fa-brands fa-square-facebook text-5xl"></i>
-            <i className="fa-brands fa-square-instagram text-5xl"></i>
-          </p>
+          <Contacto />
         </div>
 
         <div className="grow bg-slate-400 w-2/4 pt-4 ">
